db5e25a017d00f48a69870c1408e7670f7c1f03b
chtotonetak <madlabman@mail.ru>
2018-07-29 01:01:48 +0400

    Blacklist UI state for persistence

--- src/store/store.js
+++ src/store/store.js
@@ -14,6 +14,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
     key: 'root',
     storage: AsyncStorage,
+    blacklist: ['ui']
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
